0c76b99d8beb661ea38deecd57c7217757f36fbe
Rinz-Code <rinshinsmails123@gmail.com>
2021-01-27 14:40:39 +0530

    merge brach sass

--- todo/base/views.py
+++ todo/base/views.py
@@ -1,8 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
-# Create your views here.
-tasks = []
+
+from .forms import NameForm
+
+
 
 def viewtasks(request):
+    if "tasks"  not in request.session:
+        request.session["tasks"] = []
     template_name = 'base/view.html'
+    tasks = request.session["tasks"]
+    taskss = enumerate(tasks)
+    context = {"tasks": taskss}
     return render(request,template_name,context)
+
+def add(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = NameForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            task = form.cleaned_data['task']
+            # redirect to a new URL:
+            if task:
+                request.session["tasks"] += [task]
+            return redirect('viewtasks')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = NameForm()
+
+    return render(request, 'base/add.html', {'form': form})
+# Create your views here.
+
+
