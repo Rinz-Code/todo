a5e130555ce02f912840550879746c943538f19c
Rinz-Code <rinshinsmails123@gmail.com>
2021-01-27 11:53:19 +0530

    merge brach sass

--- todo/base/views.py
+++ todo/base/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+tasks = []
+
+def viewtasks(request):
+    template_name = 'base/view.html'
+    return render(request,template_name,context)
